5a5ca4230fd8a181a5814d20eac229dad5268efd
Welliton gervickas <wellitogervickas@gmail.com>
2021-06-13 07:15:23 -0300

    Mindmap structure

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+import Offices from '@/components/Offices'
+
+export default {
+	name: 'Index',
+	components: {
+		Offices
+	}
+}
+</script>
+
+<template>
+  	<div>
+		<offices />
+  	</div>
+</template>
+
+<style></style>
